3f63baaecb504f9d2d6bb8eec16e4f03a2dde715
Nate Wong <natewong1@gmail.com>
2023-03-14 10:00:00 -0400

    Finish error handling:

--- src/cGPT.c
+++ src/cGPT.c
@@ -56,7 +56,6 @@ static int int_key_valid(cJSON *obj){
 }
 // parse response json body
 static ChatCompletion *parse_response(char *response){
-    printf("%s\n", response);
     cJSON *parsed_json = cJSON_Parse(response);
     if (parsed_json == NULL) {
         // free mem
@@ -73,17 +72,17 @@ static ChatCompletion *parse_response(char *response){
     chat_completion->num_choices = num_choices;
     // parse id
     char *id_str = cJSON_GetStringValue(cJSON_GetObjectItem(parsed_json, "id"));
-    chat_completion->id = malloc(strlen(id_str));
+    chat_completion->id = malloc(strlen(id_str + sizeof("")));
     strcpy(chat_completion->id, id_str);
     // parse object
     char *object_str = cJSON_GetStringValue(cJSON_GetObjectItem(parsed_json, "object"));
-    chat_completion->object = malloc(strlen(object_str));
+    chat_completion->object = malloc(strlen(object_str + sizeof("")));
     strcpy(chat_completion->object, object_str);
     // parse created
     chat_completion->created = cJSON_GetNumberValue(cJSON_GetObjectItem(parsed_json, "created"));
     // parse model
     char *model_str = cJSON_GetStringValue(cJSON_GetObjectItem(parsed_json, "model"));
-    chat_completion->model = malloc(strlen(model_str));
+    chat_completion->model = malloc(strlen(model_str + sizeof("")));
     strcpy(chat_completion->model, model_str);
     // parse usage
     cJSON *usage = cJSON_GetObjectItem(parsed_json, "usage");
@@ -98,16 +97,16 @@ static ChatCompletion *parse_response(char *response){
         cJSON *message_item = cJSON_GetObjectItem(choice_item, "message");
         // parse role
         char *role_str =  cJSON_GetStringValue(cJSON_GetObjectItem(message_item, "role"));
-        chat_completion->choices[i].message.role  = malloc(strlen(role_str));
+        chat_completion->choices[i].message.role  = malloc(strlen(role_str + sizeof("")));
         strcpy(chat_completion->choices[i].message.role, role_str);
         // parse content
         char *content_str =  cJSON_GetStringValue(cJSON_GetObjectItem(message_item, "content"));
-        chat_completion->choices[i].message.content  = malloc(strlen(content_str));
+        chat_completion->choices[i].message.content  = malloc(strlen(content_str + sizeof("")));
         strcpy(chat_completion->choices[i].message.content, content_str);
 
         // parse finish_reason
         char *finish_reason_str = cJSON_GetStringValue(cJSON_GetObjectItem(choice_item, "finish_reason"));
-        chat_completion->choices[i].finish_reason  = malloc(strlen(finish_reason_str));
+        chat_completion->choices[i].finish_reason  = malloc(strlen(finish_reason_str + sizeof("")));
         strcpy(chat_completion->choices[i].finish_reason, finish_reason_str);
         // parse index
         chat_completion->choices[i].index = cJSON_GetNumberValue(cJSON_GetObjectItem(choice_item, "index"));
@@ -115,6 +114,23 @@ static ChatCompletion *parse_response(char *response){
     // free mem
     cJSON_Delete(parsed_json);
     return chat_completion;
+}
+// parse an error
+static char *parse_error(char *response){
+    cJSON *parsed_json = cJSON_Parse(response);
+    if (parsed_json == NULL) {
+        // free mem
+        cJSON_Delete(parsed_json);
+        return NULL;
+    }
+    cJSON *error_obj = cJSON_GetObjectItem(parsed_json, "error");
+    // get error message, create a pointer, set it to error message
+    char *error_msg = cJSON_GetStringValue(cJSON_GetObjectItem(error_obj, "message"));
+    char *error = malloc(strlen(error_msg) + sizeof(""));
+    strcpy(error, error_msg);
+
+    cJSON_Delete(parsed_json);
+    return error;
 }
 // Create a completion for specified chat message
 ChatCompletion *create_chat_completion(ChatParams *params){
@@ -152,12 +168,22 @@ ChatCompletion *create_chat_completion(ChatParams *params){
         curl_easy_setopt(curl, CURLOPT_WRITEDATA, (void *)&chunk);
         // send the http request
         res = curl_easy_perform(curl);
-        // analyze response code
+        // response pointer
         ChatCompletion *chat_completion;
-        if (res != CURLE_OK) {
-            fprintf(stderr, "curl_easy_perform() failed: %s\n", curl_easy_strerror(res));
-        } else {
+        // status code of request 
+        int res_code = 0;
+        curl_easy_getinfo(curl, CURLINFO_RESPONSE_CODE, &res_code);
+        int success = 0;
+        if(res_code == 200){
             chat_completion = parse_response(chunk.response);
+            success = 1;
+            if(chat_completion == NULL){
+                fprintf(stderr, "Unknown error parsing response body\n");
+                success = 0;
+            }
+        }else{
+            char *error_msg = parse_error(chunk.response);
+            fprintf(stderr, "Got status code %d, %s\n", res_code, error_msg);
         }
         // always cleanup so no memory leaks!
         curl_easy_cleanup(curl);
@@ -165,9 +191,12 @@ ChatCompletion *create_chat_completion(ChatParams *params){
         curl_global_cleanup();
         free(chunk.response);
         free(req_body);
-
-        return chat_completion;
+        if(success){
+            return chat_completion;
+        }else{
+            return NULL;
+        }
     }
     curl_global_cleanup();
-    return 0;
+    return NULL;
 }
--- src/main.c
+++ src/main.c
@@ -16,23 +16,25 @@ int main(){
         .messages_size=1
     };
     ChatCompletion *chat_completion = create_chat_completion(&params);
-    printf("Response\n");
-    printf("id: %s\nobject: %s\ncreated: %d\nmodel: %s\n", chat_completion->id, chat_completion->object, chat_completion->created, chat_completion->model);
-    printf("usage: {\n");
-    printf("    prompt_tokens: %d\n", chat_completion->usage.prompt_tokens);
-    printf("    completion_tokens: %d\n", chat_completion->usage.completion_tokens);
-    printf("    total_tokens: %d\n", chat_completion->usage.total_tokens);
-    printf("}\n");
+    if(chat_completion != NULL){
+        printf("Response\n");
+        printf("id: %s\nobject: %s\ncreated: %d\nmodel: %s\n", chat_completion->id, chat_completion->object, chat_completion->created, chat_completion->model);
+        printf("usage: {\n");
+        printf("    prompt_tokens: %d\n", chat_completion->usage.prompt_tokens);
+        printf("    completion_tokens: %d\n", chat_completion->usage.completion_tokens);
+        printf("    total_tokens: %d\n", chat_completion->usage.total_tokens);
+        printf("}\n");
 
-    for (int i = 0; i < chat_completion->num_choices; i++) {
-        printf("Choice %d:\n", i+1);
-        printf("Message role: %s\n", chat_completion->choices[i].message.role);
-        printf("Message content: %s\n", chat_completion->choices[i].message.content);
-        printf("Finish reason: %s\n", chat_completion->choices[i].finish_reason);
-        printf("Index: %d\n", chat_completion->choices[i].index);
-        printf("\n");
+        for (int i = 0; i < chat_completion->num_choices; i++) {
+            printf("Choice %d:\n", i+1);
+            printf("Message role: %s\n", chat_completion->choices[i].message.role);
+            printf("Message content: %s\n", chat_completion->choices[i].message.content);
+            printf("Finish reason: %s\n", chat_completion->choices[i].finish_reason);
+            printf("Index: %d\n", chat_completion->choices[i].index);
+            printf("\n");
+        }
+        free(chat_completion->choices);
+        free(chat_completion);
     }
-    free(chat_completion->choices);
-    free(chat_completion);
     return 0;
 }
