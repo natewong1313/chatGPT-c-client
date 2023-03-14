b090398c63f80ada56ebaf244d5e47f1b421b2cf
Nate Wong <natewong1@gmail.com>
2023-03-14 10:29:46 -0400

    Forgot to include this

--- include/cGPT.h
+++ include/cGPT.h
@@ -2,29 +2,73 @@
 #include "cJSON.h"
 
 typedef struct {
-    char* apiKey;
-} ChatClient;
+    char *role;
+    char *content;
+} ChatMessage;
 
 typedef struct {
-    char* role;
-    char* content;
-} ChatMessage;
+    char *apikey;
+    char model[50];
+    ChatMessage *messages;
+    int messages_size;
+} ChatParams;
 
 typedef struct {
     char *response;
     size_t size;
 } ResponseMemory;
 
+// typedef struct {
+//     char *role;
+//     char *content;
+// } Message;
+
+// typedef struct {
+//     int index;
+//     Message *message;
+//     char *finish_reason;
+// } Choice;
+
+// typedef struct {
+//     char *id;
+//     char *object;
+//     int created;
+//     char *model;
+//     Choice *choices;
+//     int choices_size;
+// } ChatResponse;
+
 typedef struct {
-    char* id;
-    char* object;
+    int prompt_tokens;
+    int completion_tokens;
+    int total_tokens;
+} Usage;
+
+typedef struct {
+    char *role;
+    char *content;
+} Message;
+
+typedef struct {
+    Message message;
+    char *finish_reason;
+    int index;
+} Choice;
+
+typedef struct {
+    char *id;
+    char *object;
     int created;
-    char* model;
-} ChatResponse;
+    char *model;
+    Usage usage;
+    Choice *choices;
+    int num_choices;
+} ChatCompletion;
 
 static size_t write_memory_callback(void *contents, size_t size, size_t nmemb, void *userp);
-static char *build_req_body(char *model, ChatMessage *messages, int messagesSize);
-static int jsonStrKeyFound(cJSON* obj);
-static int jsonIntKeyFound(cJSON* obj);
-static ChatResponse *parse_response(char *response);
-ChatResponse *create_chat_completion(char *apiKey, char *model, ChatMessage *messages, int messagesSize);
+static char *build_req_body(char *model, ChatMessage *messages, int messages_size);
+static int str_key_valid(cJSON *obj);
+static int int_key_valid(cJSON *obj);
+static ChatCompletion *parse_response(char *response);
+static char *parse_error(char *response);
+ChatCompletion *create_chat_completion(ChatParams *params);
